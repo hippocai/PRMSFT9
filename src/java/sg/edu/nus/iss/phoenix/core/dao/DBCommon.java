046de6713b99d2aad0259d7d98663f2f39a9070c
hippo <hippo_cai@sina.com>
2016-09-25 14:49:45 +0800

    shujuk

--- src/java/sg/edu/nus/iss/phoenix/core/dao/DBCommon.java
+++ src/java/sg/edu/nus/iss/phoenix/core/dao/DBCommon.java
@@ -2,6 +2,6 @@ package sg.edu.nus.iss.phoenix.core.dao;
 public class DBCommon {
 	public static final String DB_USER="root";
 	public static final String DB_PASSWORD="1993812";
-	public static final String DB_URL="jdbc:mysql://localhost:3306/phoenix";
+	public static final String DB_URL="jdbc:mysql://caiyicheng.damnserver.com:3306/phoenix";
 	public static final String DB_DRIVER="com.mysql.jdbc.Driver";
 }
